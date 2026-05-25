143da3f7e3ca1fa8a37cbd4a0db08ff9a0e87639
DuongAnh1212 <tomnguyen6766@gmail.com>
2026-05-24 17:20:56 -0700

    update(Codefore): update round 1074 div D

---
+++ Codeforce/Round1074 (Div4)/D.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(const vector<int>& base, const vector<int>& val, const vector<int>& ver, int curVer) {
+    int n = (int)base.size();
+    for (int i = 0; i < n; i++) {
+        if (ver[i] == curVer) cout << val[i] << ' ';
+        else cout << base[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m, h;
+        cin >> n >> m >> h;
+
+        vector<int> base(n);
+        for (int &a : base) cin >> a;
+
+        vector<int> val(n);
+        vector<int> ver(n, -1);
+        int curVer = 0;
+
+        for (int i = 0; i < m; i++) {
+            int b_i, c_i;
+            cin >> b_i >> c_i;   // 0-indexed
+
+            int current = (ver[b_i-1] == curVer ? val[b_i-1] : base[b_i-1]);
+
+            if (current + c_i > h) {
+                curVer++;  // lazy reset to original array
+            } else {
+                val[b_i-1] = current + c_i;
+                ver[b_i-1] = curVer;
+            }
+        }
+
+        display(base, val, ver, curVer);
+    }
+
+    return 0;
+}
